691ebcaa7aaef5854a43a2748ed8de2641ade580
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-05 18:14:58 +0530

    using stack to arrange View

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20){
+            Text("Hello, World!")
+            Text("Hello Ashutosh")
+            Text("Hello Willskhalifa")
+            HStack{
+                Text("My name is")
+                Text("Goldy")
+            }
+            ZStack{
+                Text("The First one")
+                Text("The Second one")
+                Text("It is used for depth")
+            }
+        }
+        
     }
 }
 
