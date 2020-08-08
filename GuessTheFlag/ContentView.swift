56cff76d9c17796329d22ba924dea1662b8958b8
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-08 08:02:24 +0530

    add score feature

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,20 +9,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert : Bool = false
+    
+    @State private var countries = ["India","Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showTitle = ""
+    @State private var showingScore = false
+    @State private var score = 0
     
     var body: some View {
-
-        Button("Show Alert"){
-            self.showAlert = true
-        }.alert(isPresented: $showAlert){
-            Alert(title: Text("hello Ashutosh"), message: Text("Learning SwiftUI by HackingWithSwift"), dismissButton: .default(Text("OK")))
+        
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 40){
+                VStack{
+                    Text("Tap to Flag Off")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                
+                    ForEach(0 ..< 4) {number in
+                        Button(action:{
+                            self.flagTapped(number)
+                        }){
+                            Image(self.countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .overlay(Capsule()
+                                    .stroke(Color.black, lineWidth: 1))
+                                .shadow(color: .black, radius: 2)
+                        }
+                    }
+                    Spacer()
+            
+                    Text("Your Score is : \(score)")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                }
+            }
         }
-        
-        
+        .alert(isPresented: $showingScore){
+            Alert(title: Text(showTitle), message: Text("Your Score is \(score)"), dismissButton: .default(Text("Continue")){
+                self.askQuestion()
+                })
         }
         
     }
+    
+    
+    func flagTapped(_ number: Int){
+        if (number == correctAnswer) {
+            showTitle = "Correct"
+            score += 1
+        }else{
+            showTitle = "Wrong"
+            score -= 1
+        }
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
